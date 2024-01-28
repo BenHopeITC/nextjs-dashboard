3bd25ace02941473426259fe7ce79ebb230b4675
Ben Hope <benhope.itc@gmail.com>
2024-01-28 14:03:35 +0000

    Chapters 13, Handling Errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -18,33 +18,39 @@ const FormSchema = z.object({
 // Use Zod to update the expected types
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
- 
+
 export async function createInvoice(formData: FormData) {
   // For complex forms use below:
   //const rawFormData = Object.fromEntries(formData.entries())
-
-//   const rawFormData = {
-//     customerId: formData.get('customerId'),
-//     amount: formData.get('amount'),
-//     status: formData.get('status'),
-//   };
-//   // Test it out:
-//   console.log(rawFormData);
-//   console.log(typeof rawFormData.amount);
+  //const rawFormData = {
+  //  customerId: formData.get('customerId'),
+  //  amount: formData.get('amount'),
+  //  status: formData.get('status'),
+  //};
+  //// Test it out:
+  //console.log(rawFormData);
+  //console.log(typeof rawFormData.amount);
 
   const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+ 
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
-
-  await prisma.$executeRaw`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (CAST(${customerId} as uuid), ${amountInCents}, ${status}, CAST(${date} as date))
-  `;
-
+ 
+  try {
+    await prisma.$executeRaw`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (CAST(${customerId} as uuid), ${amountInCents}, ${status}, CAST(${date} as date))
+    `;
+  } catch (error) {
+    return {
+      message: 'Database Error: Failed to Create Invoice.',
+    };
+  }
+ 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
@@ -58,17 +64,29 @@ export async function updateInvoice(id: string, formData: FormData) {
  
   const amountInCents = amount * 100;
  
-  await prisma.$executeRaw`
-    UPDATE invoices
-    SET customer_id = CAST(${customerId} as UUID), amount = ${amountInCents}, status = ${status}
-    WHERE id = CAST(${id} as UUID)
-  `;
+  try {
+    await prisma.$executeRaw`
+      UPDATE invoices
+      SET customer_id = CAST(${customerId} as UUID), amount = ${amountInCents}, status = ${status}
+      WHERE id = CAST(${id} as UUID)
+    `;
+  } catch (error) {
+    return { message: 'Database Error: Failed to Update Invoice.' };
+  }
  
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
-  await prisma.$executeRaw`DELETE FROM invoices WHERE id = CAST(${id} as UUID)`;
-  revalidatePath('/dashboard/invoices');
+  // throw new Error('Failed to Delete Invoice 1');
+  
+  try {
+    // throw new Error('Failed to Delete Invoice 2');
+    await prisma.$executeRaw`DELETE FROM invoices WHERE id = CAST(${id} as UUID)`;
+    revalidatePath('/dashboard/invoices');
+    return { message: 'Deleted Invoice.' };
+  } catch (error) {
+    return { message: 'Database Error: Failed to Delete Invoice.' };
+  }
 }
